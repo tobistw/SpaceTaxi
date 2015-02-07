7fc8fe0b5a3cff9d95f7e140ba0e796c7f7b3b2c
aKipp <andrea_kipp@web.de>
2015-02-07 22:51:23 +0100

    Weltallschrott, Taxiplattform

---
+++ UnityProject/Assets/Scripts/Umweltschrott.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Umweltschrott : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.name == "taxi") {
+			if (collision.relativeVelocity.magnitude > 5) {
+
+				TaxiManager.instance.Damage = TaxiManager.instance.Damage - collision.relativeVelocity.magnitude;
+			}
+		}
+		
+		//Debug.Log (PlayerPrefs.GetFloat ("Damage"));
+		//Debug.Log (collision.relativeVelocity.magnitude);
+		//if (collision.relativeVelocity.magnitude > 2)
+		//	audio.Play();
+		
+	}
+}
---
+++ UnityProject/Assets/Scripts/UmweltschrottSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UmweltschrottSpawn : MonoBehaviour
+{
+	public float spawnTime = 15f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public Rigidbody2D[] enemies;		// Array of enemy prefabs.
+	public float spawndirectionX = 6F; 
+	public float spawndirectionY = -5F; 
+	
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, Random.Range(10F, 25F));
+	}
+	
+	//Random.Range(-10.0F, 10.0F)
+	void Spawn ()
+	{
+		int enemyIndex = Random.Range(0, enemies.Length);
+		Rigidbody2D enemieInstance;
+		enemieInstance = Instantiate (enemies [enemyIndex], transform.position, transform.rotation) as Rigidbody2D;
+		enemieInstance.AddForce(new Vector2(Random.Range(spawndirectionX - 4F,spawndirectionX + 4F), Random.Range(spawndirectionY - 4F, spawndirectionY + 4F)) * Random.Range(80, 160));
+
+	}
+}
