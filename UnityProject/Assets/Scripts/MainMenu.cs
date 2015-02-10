e6fa87f335e1667e5a8aa37000dbf049bb5635e2
tbergman <tobias.bergmann@studmail.w-hs.de>
2015-02-10 01:09:17 +0100

    lauffähige Version_02

--- UnityProject/Assets/Scripts/MainMenu.cs
+++ UnityProject/Assets/Scripts/MainMenu.cs
@@ -41,14 +41,7 @@ public class MainMenu : MonoBehaviour {
 		//Playerprefs bis auf highscore zurücksetzten 
 
 		levelStats.GameIsRunning = true;
-<<<<<<< HEAD
 		Debug.Log("läuft ein Spiel?: " +  levelStats.GameIsRunning);
-		
-		Destroy (TaxiManager.instance.gameObject);
-		Application.Quit ();
-=======
-		Application.Quit ();
-
 
 		/*if (TaxiManager.instance.gameObject != null || OrbManager.instance.gameObject != null || PassengerManager.instance.gameObject != null) {
 			Destroy (TaxiManager.instance.gameObject);
@@ -56,7 +49,6 @@ public class MainMenu : MonoBehaviour {
 			Destroy (PassengerManager.instance.gameObject);
 		}*/
 
->>>>>>> prototyp_v01
 		Application.LoadLevel (startLevelIndex);
 
 
